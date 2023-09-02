f1219c319afd75577eafecf674cae1917bcb9dd2
haejin <haejin.yang91@gmail.com>
2023-09-02 15:45:15 +0900

    feat: 작업 1. users 모델 추가 (#4)

---
+++ app/Models/Host.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Models;
+
+class Host extends User
+{
+    protected $table = 'users';
+}
---
+++ app/Models/Participant.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Models;
+
+class Participant extends User
+{
+    protected $table = 'users';
+}
