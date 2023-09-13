ea2d5c447a66f062a2e6d3542b7f53d1a16372b0
haejin <haejin.yang91@gmail.com>
2023-09-13 19:09:13 +0900

    feat: 작업 2. 유저에 속한 토픽을 응답하는 컨트롤러 추가  (#9)

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\Opinion\OpinionController;
 use App\Http\Controllers\Test\TestController;
 use App\Http\Controllers\Topic\TopicController;
 use App\Http\Controllers\Topic\TopicOpinionController;
+use App\Http\Controllers\User\UserTopicController;
+use App\Http\Controllers\User\UserTopicOpinionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,7 +29,8 @@ Route::get('/', [TestController::class, 'test']);
 Route::apiResource('topics', TopicController::class)->only('index', 'show');
 Route::get('topics/{topic}/opinions', TopicOpinionController::class);
 Route::apiResource('opinions', OpinionController::class)->only('store', 'show');
-
+Route::apiResource('users/{user}/topics/{topic}/opinions', UserTopicOpinionController::class)->only('index');
+Route::apiResource('users/{user}/topics', UserTopicController::class)->only('index');
 // fallback은 라우터 가장 하단에 있어야 한다.
 Route::fallback(function () {
     return response()->json("등록되지 않은 URL입니다.");
