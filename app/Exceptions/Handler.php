f9e01b88572b6b74d87e9620c3a1d12d5ec0df83
haejin <haejin.yang91@gmail.com>
2023-09-14 18:48:16 +0900

    feat: 작업 5. API응답용 에러 핸들러 추가 (#8)

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,20 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponser;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
+
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
@@ -26,5 +35,36 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if ($e instanceof ModelNotFoundException) {
+            $model = strtolower(class_basename($e->getModel()));
+
+            return $this->error("{$model}이 존재하지 않습니다", Response::HTTP_NOT_FOUND);
+        }
+
+        if ($e instanceof ValidationException) {
+            return $this->error($e->getMessage(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        if ($e instanceof AuthorizationException) {
+            return $this->error($e->getMessage(), Response::HTTP_FORBIDDEN);
+        }
+
+        if ($e instanceof RouteNotFoundException) {
+            return $this->error($e->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+
+        if ($e instanceof QueryException) {
+            return $this->error($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        if (config('app.debug')) {
+            return parent::render($request, $e);
+        }
+
+        return $this->error('예기치 못한 예외', Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
