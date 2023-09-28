6f3b9b978bb77c6c5710fb19a4e430bb7f285192
haejin <haejin.yang91@gmail.com>
2023-09-28 20:33:07 +0900

    feat: 작업 1. 배열 안의 키를 확인할 수 있는 헬퍼 함수를 생성 (#16)

---
+++ app/Util/ArrayUtil.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Util;
+
+class ArrayUtil
+{
+    /**
+     * 배열안에 키가 있는지 확인. 배열안의 키가 있지만 그 값이 null인경우에도 true 반환
+     * @param array $keys 확인하고 싶은 키 배열
+     * @param array $array 키가 있는지 확인할 배열
+     * @return bool
+     */
+    public static function existKeys(array $keys, array $array)
+    {
+        foreach ($keys as $key) {
+            if (!array_key_exists($key, $array)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * 배열안에 키가 있는지 확인. 배열안의 키가 있지만 그 값이 null이라면 false 반환
+     * @param array $keys 확인하고 싶은 키 배열
+     * @param array $array 키가 있는지 확인할 배열
+     * @return bool
+     */
+    public static function existKeysStrictly(array $keys, array $array)
+    {
+        foreach ($keys as $key) {
+            if (!isset($array[$key])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
