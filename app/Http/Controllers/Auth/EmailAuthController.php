46b8780161c63742bcc3de1264b3527bf8ee219a
haejin <haejin.yang91@gmail.com>
2023-10-24 14:57:07 +0900

    feat: 작업 7. 이메일로 보낸 링크를 클릭해서 인증 처리하는 컨트롤러 구현 (#22)

--- app/Http/Controllers/Auth/EmailAuthController.php
+++ app/Http/Controllers/Auth/EmailAuthController.php
@@ -3,21 +3,35 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Util\ArrayUtil;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class EmailAuthController extends Controller
 {
     public function __invoke(Request $request)
     {
         $input = $request->input();
-        assert(ArrayUtil::existKeysStrictly(['user', 'token'], $input), 'field required');
 
-        $user = $input['user'];
+        if (!(isset($input['user']) && isset($input['token']))) {
+            return view('mail.verify', ['msg' => "양식에 맞지 않습니다."]);
+        }
+
+        $email = $input['user'];
         $token = $input['token'];
 
-        // TODO: 인증진행해야함.
-        
-        return view('mail.verify', ['user' => $user, 'token' => $token]);
+        $user = User::where('email', $email)->firstOrFail();
+        if ($user->email_verified_at) {
+            return view('mail.verify', ['msg' => "이미 인증된 계정입니다."]);
+        }
+
+        if ($user->remember_token !== $token) {
+            return view('mail.verify', ['msg' => "인증을 실패했습니다."]);
+        }
+
+        $user->email_verified_at = Carbon::now();
+        $user->save();
+
+        return view('mail.verify', ['msg' => "인증을 완료했습니다."]);
     }
 }
