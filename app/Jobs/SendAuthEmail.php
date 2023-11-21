582fe235089220fa29e8fbfae10ba48986c37524
HaejinYang <haejin.yang91@gmail.com>
2023-11-21 20:46:26 +0900

    feat: 작업 1. AuthController의 register에서 인증메일 보내는 것을 job으로 변경 (#28)

---
+++ app/Jobs/SendAuthEmail.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\AuthMail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class SendAuthEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(private string $email, private string $rememberToken)
+    {
+
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        if (env('APP_ENV') === 'local') {
+            Mail::to('extension.master.91@gmail.com')->send(new AuthMail($this->email, $this->rememberToken));
+        } else {
+            Mail::to($this->email)->send(new AuthMail($this->email, $this->rememberToken));
+        }
+    }
+}
