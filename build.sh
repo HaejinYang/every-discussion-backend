8028775f4ab2e1e69760050224a2d39b263fce32
haejin.yang91 <haejin.yang91@gmail.com>
2024-01-07 02:08:43 +0900

    feat: 작업 1. 2. .env를 분리하고 프로젝트 셋업에서 다시 합치도록 스크립트 및 env 수정 (#44)
    기존 .env는 .env.core가 되고 production과 local 변경해야할 부분은 각각 .env.lcoal, .env.production에 저장해서 쓰면된다. docker compose가 실행될때 스크립트가 실행되어 .env 파일 하나로 뽑아준다.

---
+++ build.sh
@@ -0,0 +1,12 @@
+source ./.env.core
+
+rm -f ./.env
+
+if [ "$APP_ENV" = "production" ]; then
+    rm -f /usr/local/etc/php/conf.d/xdebug.ini
+    cat ./.env.core ./.env.production > ./.env
+else
+    rm -f /usr/local/etc/php/conf.d/opcache.ini
+    rm -f /usr/local/etc/php/conf.d/jit.ini
+    cat ./.env.core ./.env.local > ./.env
+fi
