4fcc173b520c6b03fa8cafab0d37e226d889b3f1
haejin.yang91 <haejin.yang91@gmail.com>
2024-01-11 11:09:21 +0900

    feat: 작업 2. 참여한 토론 simplePagenation (#50)

--- app/Http/Controllers/User/UserTopicController.php
+++ app/Http/Controllers/User/UserTopicController.php
@@ -5,22 +5,35 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\ApiController;
 use App\Http\Requests\UserTopic\UserTopicShowRequest;
 use App\Models\Participant;
-use App\Models\Topic;
+use App\Services\Topic\TopTopicsService;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class UserTopicController extends ApiController
 {
-    public function __invoke(Participant $user)
+    public function __invoke(Request $request, Participant $user)
     {
         assert(!is_null($user), '유저 필드가 필요합니다.');
+        $keyword = $request->input('keyword');
+        $perPage = 5;
 
+        // 유저가 생성한 토픽
+        $result = DB::select("SELECT id FROM topics WHERE user_id = :user_id", ['user_id' => $user->id]);
+        $topicsOfHost = array_column($result, 'id');
+
+        // 유저가 참여한 토픽
         $topicIds = $user->topics()->pluck('id');
-        $topicsOfHost = DB::select("SELECT id FROM topics WHERE user_id = :user_id", ['user_id' => $user->id]);
-        $topicsOfHost = array_column($topicsOfHost, 'id');
         $topicIds = array_merge($topicIds->toArray(), $topicsOfHost);
         $topicIds = array_unique($topicIds);
 
-        $topics = Topic::whereIn('id', $topicIds)->get();
-        return $this->showAll($topics);
+        if (is_null($keyword)) {
+            $topicsPaginator = TopTopicsService::getTopTopicsWithIds($topicIds, $perPage);
+
+            return $this->showAll($topicsPaginator);
+        }
+
+        $topicsPaginator = TopTopicsService::getTopTopicsWithIdsAndKeyword($keyword, $topicIds, $perPage);
+
+        return $this->showAll($topicsPaginator);
     }
 }
--- app/Services/Topic/TopTopicsService.php
+++ app/Services/Topic/TopTopicsService.php
@@ -17,4 +17,14 @@ class TopTopicsService
         // options 파라미터에 ['mode' => 'boolean']처럼 줘야 한다.
         return Topic::whereFullText('title', $keyword . "*", ['mode' => 'boolean'])->orderBy('title')->simplePaginate($perPage);
     }
+
+    public static function getTopTopicsWithIds(array $ids, int $perPage)
+    {
+        return Topic::whereIn('id', $ids)->simplePaginate($perPage);
+    }
+
+    public static function getTopTopicsWithIdsAndKeyword(string $keyword, array $ids, int $perPage)
+    {
+        return Topic::whereIn('id', $ids)->whereFullText('title', $keyword . "*", ['mode' => 'boolean'])->orderBy('title')->simplePaginate($perPage);
+    }
 }
