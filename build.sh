79b6de917c4922685b3f8ef24fe4b14a807af9ca
haejin.yang91 <haejin.yang91@gmail.com>
2024-01-16 04:08:22 +0900

    feat: 작업 2. debug, production을 기반으로 빌드 파일 수정 (#52)

--- build.sh
+++ build.sh
@@ -3,11 +3,10 @@ source ./.env.core
 rm -f ./.env
 
 if [ "$APP_ENV" = "production" ]; then
-    rm -f /usr/local/etc/php/conf.d/xdebug.ini
+    rm -f /usr/local/etc/php/conf.d/debug.ini
     cat ./.env.core ./.env.production > ./.env
 else
-    rm -f /usr/local/etc/php/conf.d/opcache.ini
-    rm -f /usr/local/etc/php/conf.d/jit.ini
+    rm -f /usr/local/etc/php/conf.d/production.ini
     cat ./.env.core ./.env.local > ./.env
 fi
 
