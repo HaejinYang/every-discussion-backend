6d8828feff870178f4d7b89f3db7c86c29754eb4
haejin.yang91 <haejin.yang91@gmail.com>
2024-01-18 04:21:56 +0900

    chore: production 모드에 디버그를 추가한 release모드 추가 (#56)

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ rm -f ./.env
 if [ "$APP_ENV" = "production" ]; then
     rm -f /usr/local/etc/php/conf.d/debug.ini
     cat ./.env.core ./.env.production > ./.env
+elif [ "$APP_ENV" = "release" ]; then
+        rm -f /usr/local/etc/php/conf.d/debug.ini
+        cat ./.env.core ./.env.release > ./.env
 else
     rm -f /usr/local/etc/php/conf.d/production.ini
     cat ./.env.core ./.env.local > ./.env
